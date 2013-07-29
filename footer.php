035c8aef4c55800907209ba937e537f4eb9e2863
ddobrev81 <ddobrev81@gmail.com>
2013-07-29 13:35:47 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+</div>
+  
+	<div id="footer">
+		<p>Copyright &copy; <a href="#">Dodo</a> 2007 | Designed by Babatitrankena | Sponsored by <a href="http://www.opendesigns.org/">Open Designs</a> | Valid <a href="http://jigsaw.w3.org/css-validator/">CSS</a> &amp; <a href="http://validator.w3.org/">XHTML</a></p>
+	</div>
+</body>
+</html>
