1a43189f87535851cbc0bbf4aa34a9087b8d8b90
ddobrev81 <ddobrev81@gmail.com>
2013-07-29 13:18:35 +0300

    Create dbc.php
    Database connection using PDO

---
+++ dbc.php
@@ -0,0 +1,15 @@
+<?php #dbc.php
+
+$dbc = "mysql:host=mysql8.000webhost.com;dbname=a4962218_learn";
+$login="a4962218_ddod";
+$password="xaxaxa123";
+$opt = array(
+    // any occurring errors wil be thrown as PDOException
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    // an SQL command to execute when connecting
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
+);
+
+$pdo = new PDO($dbc, $login, $password, $opt);
+
+?>
